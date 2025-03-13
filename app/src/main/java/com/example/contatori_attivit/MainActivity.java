480b8581b3910793378741efb7bda756828b87a9
N1K006 <163847933+N1K006@users.noreply.github.com>
2025-03-13 10:19:48 +0100

    da progetto Android

--- app/src/main/java/com/example/contatori_attivit/MainActivity.java
+++ app/src/main/java/com/example/contatori_attivit/MainActivity.java
@@ -8,10 +8,41 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity
+{
+    // Variabili per tenere traccia dei conteggi
+    private int q_lavoro = 0;
+    private int q_sport = 0;
+    private int q_acqua_bevuta = 0;
+
+    // TextView per visualizzare i conteggi
+    private TextView txtQuantitàLavoro;
+    private TextView txtQuantitàSport;
+    private TextView txtQuantitàAcquaBevuta;
+
+    // TextView per lo storico
+    private TextView txtStoricoLavoro;
+    private TextView txtStoricoSport;
+    private TextView txtStoricoAcquaBevuta;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -20,5 +51,74 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Inizializzazione delle TextView per i conteggi
+        txtQuantitàLavoro = findViewById(R.id.txt_quantità_lavoro);
+        txtQuantitàSport = findViewById(R.id.txt_quantità_sport);
+        txtQuantitàAcquaBevuta = findViewById(R.id.txt_quantità_acqua_bevuta);
+
+        // Inizializzazione delle TextView per lo storico
+        txtStoricoLavoro = findViewById(R.id.txt_storico_lavoro);
+        txtStoricoSport = findViewById(R.id.txt_storico_sport);
+        txtStoricoAcquaBevuta = findViewById(R.id.txt_storico_acqua_bevuta);
+
+        /*// Inizializzazione dei bottoni
+        Button bLavoroPiù = findViewById(R.id.b_lavoro_più);
+        Button bLavoroMeno = findViewById(R.id.b_lavoro_meno);
+
+        Button bSportPiù = findViewById(R.id.b_sport_più);
+        Button bSportMeno = findViewById(R.id.b_sport_meno);
+
+        Button bAcquaBevutaPiù = findViewById(R.id.b_acqua_bevuta_più);
+        Button bAcquaBevutaMeno = findViewById(R.id.b_acqua_bevuta_meno);*/
+
+    }
+
+    public void b_lavoro(View v)
+    {
+        Button b = (Button) v;
+
+        if (b.getText().equals("+"))
+            q_lavoro++;
+        else
+        {
+            if (q_lavoro > 0)
+                q_lavoro--;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.getDefault());
+        txtQuantitàLavoro.setText("Quantità: " + q_lavoro);
+        txtStoricoLavoro.setText(sdf.format(new Date()));
+    }
+
+    public void b_sport(View v)
+    {
+        Button b = (Button) v;
+
+        if (b.getText().equals("+"))
+            q_sport++;
+        else
+        {
+            if (q_sport > 0)
+                q_sport--;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.getDefault());
+        txtQuantitàSport.setText("Quantità: " + q_sport);
+        txtStoricoSport.setText(sdf.format(new Date()));
+    }
+
+    public void b_acqua_bevuta(View v)
+    {
+        Button b = (Button) v;
+
+        if (b.getText().equals("+"))
+            q_acqua_bevuta++;
+        else
+        {
+            if (q_acqua_bevuta > 0)
+                q_acqua_bevuta--;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.getDefault());
+        txtQuantitàAcquaBevuta.setText("Quantità: " + q_acqua_bevuta);
+        txtStoricoAcquaBevuta.setText(sdf.format(new Date()));
     }
 }
